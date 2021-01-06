a002cd02228686c8e9097572a4b042dbe7fe7833
Artem Moshnin <tap.kap.tap@gmail.com>
2021-01-06 08:57:56 +0100

    configured typescript

---
+++ build/block.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Block = /** @class */ (function () {
+    function Block() {
+        console.log("dlskamdska");
+    }
+    return Block;
+}());
+exports.default = Block;
---
+++ src/block.ts
@@ -0,0 +1,5 @@
+export default class Block {
+  constructor() {
+    console.log("dlskamdska")
+  }
+}
